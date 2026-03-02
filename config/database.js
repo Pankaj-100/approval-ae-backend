c7b96026b5a231ab7a94a15281b9e1aa3bec4e34
pankaj-100 <pankajtech1234@gmail.com>
2026-03-02 10:00:21 +0530

    update db connection

--- config/database.js
+++ config/database.js
@@ -3,8 +3,7 @@ const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 
 module.exports = () => {
-  mongoose
-    .connect(process.env.MONGO_URL)
+ mongoose.connect(process.env.DATABASE_URL)
     .then((data) => {
       console.log(`Database server connected at port: ${data.connection.port}`);
       console.log(`Database server connected at host: ${data.connection.host}`);
